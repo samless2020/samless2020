3828b8ba0bf66a2a1df2c1eff3eeb6ac7c13b920
samless2020 <64289067+samless2020@users.noreply.github.com>
2020-04-25 06:57:20 +0800

    Update main.php

--- main.php
+++ main.php
@@ -1,1 +1,16 @@
 include 'db.php';
+$btoken=isset($_cookie['btoken'])?:$_cookie['btoken']fasle;
+$mtoken=isset($_cookie['mtoken'])?:$_cookie['mtoken']fasle;
+$utoken=isset($_cookie['utoken'])?:$_cookie['utoken']fasle;
+if($btoken){
+  if($utoken){
+
+  }  
+  else{
+
+
+  }
+
+}else{
+  return 100;
+}
